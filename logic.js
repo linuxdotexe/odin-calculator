a3866b7fc85d01772daad82f5e214dee60a7664c
sai nivas mangu <sainonbeat99@gmail.com>
2024-05-10 10:27:54 +0530

    add: operate function

--- logic.js
+++ logic.js
@@ -1,3 +1,19 @@
+let operandA;
+let operandB;
+let operator;
+
+function operate(operandA, operandB, operator) {
+  if (operator === "+") {
+    return add(operandA, operandB);
+  } else if (operator === "-") {
+    return subtract(operandA, operandB);
+  } else if (operator === "*") {
+    return multiply(operandA, operandB);
+  } else {
+    return divide(operandA, operandB);
+  }
+}
+
 function add(a, b) {
   return a + b;
 }
